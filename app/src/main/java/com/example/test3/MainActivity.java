4f8d27538a6f8a1d22d18dc67a1847e856f86230
ivan_kolosov <kolivim@yandex.ru>
2026-02-15 18:59:23 +0300

    Реализовано создание записи на основании переданных пользователем данных

--- app/src/main/java/com/example/test3/MainActivity.java
+++ app/src/main/java/com/example/test3/MainActivity.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -17,28 +18,210 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.test3.expenseList.Expense;
+import com.example.test3.listTest.UserAccount;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 
 public class MainActivity extends /* ListActivity */  AppCompatActivity {
 
     /** Реализация с extends AppCompatActivity v2 : */
+    /*
     // набор данных, который свяжем со списком
     String[] countries = { "Бразилия", "Аргентина", "Колумбия", "Чили", "Уругвай"};
+    */
     /** !Реализация с extends AppCompatActivity v2 : */
 
 
     /** Реализация с extends AppCompatActivity v3 : */
+    /*
     ArrayList<String> users = new ArrayList<String>();
     ArrayList<String> selectedUsers = new ArrayList<String>();
     ArrayAdapter<String> adapter;
     ListView usersList;
+    */
     /** !Реализация с extends AppCompatActivity v3 */
 
 
-//    private EditText editTextName;
-//    private TextView textViewUsers;
-//    private UserContract.Presenter presenter;
+    /** Реализация с extends AppCompatActivity v4 : (UserAccount) */
+    /*
+    ArrayAdapter<UserAccount> arrayAdapter;
+    ListView listView;
+    ArrayList<UserAccount> selectedUsers = new ArrayList<UserAccount>();
+    */
+    /** !Реализация с extends AppCompatActivity v4 */
+
+
+    /** Реализация с extends AppCompatActivity v4 : (Expense) */
+    ArrayAdapter<Expense> arrayAdapter;
+    ListView listView;
+    ArrayList<Expense> selectedUsers = new ArrayList<Expense>();
+    /** !Реализация с extends AppCompatActivity v4 */
+
+
+
+    /** Реализация с extends AppCompatActivity v4 : (Expense) */
+//    /*
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.expenseList);
+
+
+        ArrayList<Expense> expenseList = new ArrayList<Expense>() {{
+            add(new Expense("Lunch"));
+            add(new Expense("Coffee"));
+            add(new Expense("Supper"));
+        }};
+
+
+        arrayAdapter = new ArrayAdapter<Expense>(this, android.R.layout.simple_list_item_1 , expenseList);
+
+        listView.setAdapter(arrayAdapter);
+
+
+        // Обработка установки и снятия отметки в списке :
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+
+
+//                CheckBox cb = (CheckBox) v.getTag();
+//                cb.setChecked(true);
+
+//                listView.setItemChecked(1, false);
+
+
+                // Получаем нажатый элемент
+                Expense expense = arrayAdapter.getItem(position);
+
+                if(listView.isItemChecked(position)) {
+                    selectedUsers.add(expense);
+                } else {
+                    selectedUsers.remove(expense);
+                }
+
+
+//                // снимаем все ранее установленные отметки (Убрать после написания Multiple реализации)
+//                listView.clearChoices();
+
+            }
+
+        });
+        // !Обработка установки и снятия отметки в списке
+
+    }
+
+
+    public void add(View view){
+
+        /** Вычитываем введённые пользователем данные: */
+        EditText expenseNameEditText = findViewById(R.id.editTextNameExpense);
+        String expenseName = expenseNameEditText.getText().toString();
+
+        EditText expenseEditText = findViewById(R.id.editTextNumberDecimal);
+        Double expense = Double.parseDouble(expenseEditText.getText().toString());
+
+        EditText expenseDateEditText = findViewById(R.id.editTextDate);
+        String expenseDateTimeString = expenseDateEditText.getText().toString();
+
+        String expenseDescription = null;
+
+        /** Создаём новую запись: */
+        if(!expenseName.isEmpty()){
+
+//            Expense newExpense = new Expense(expenseName);
+            Expense newExpense = getNewExpense(expenseName, expense, expenseDateTimeString, expenseDescription);
+
+            arrayAdapter.add(newExpense);
+            cleanUserInput(expenseNameEditText, expenseEditText, expenseDateEditText);
+//            expenseNameEditText.setText("");
+//            expenseEditText.setText("");
+//            expenseDateEditText.setText("");
+            arrayAdapter.notifyDataSetChanged();
+
+        }
+
+    }
+
+
+    public void remove(View view){
+
+        // Получаем и удаляем выделенные элементы
+        for(int i=0; i< selectedUsers.size();i++){
+            arrayAdapter.remove(selectedUsers.get(i));
+        }
+
+        // Снимаем все ранее установленные отметки
+        listView.clearChoices();
+
+        // Очищаем массив выбраных объектов
+        selectedUsers.clear();
+
+        arrayAdapter.notifyDataSetChanged();
+    }
+//    */
+
+
+    public Expense getNewExpense(String expenseName, Double expense, String expenseDateTimeString, String expenseDescription) {
+
+        if(expenseName != null && !expenseName.isEmpty()) {
+
+            Expense newExpense = new Expense(expenseName);
+
+            if(expense != null && !expense.isNaN()) newExpense.addExpense(expense);
+
+            if(expenseDateTimeString != null && !expenseDateTimeString.isEmpty()) {
+                ZonedDateTime expenseZonedDateTime = getZoneDateTime(expenseDateTimeString);
+                if(expenseZonedDateTime != null) newExpense.setDateTime(expenseZonedDateTime);
+            }
+
+            if(expenseDescription != null && !expenseDescription.isEmpty()) newExpense.setDescription(expenseDescription);
+
+            return newExpense;
+        }
+
+
+        return null;
+    }
+
+
+    public ZonedDateTime getZoneDateTime(String dateString) {
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yy");
+
+        Date date = null;
+        ZonedDateTime zonedDateTime = null;
+
+        try {
+
+            date = formatter.parse(dateString);
+
+            zonedDateTime = date.toInstant().atZone(ZonedDateTime.now().getZone());
+
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+        return zonedDateTime;
+    }
+
+    public void cleanUserInput(EditText expenseNameEditText, EditText expenseEditText, EditText expenseDateEditText) {
+
+        expenseNameEditText.setText("");
+        expenseEditText.setText("");
+        expenseDateEditText.setText("");
+
+    }
+    /** !Реализация с extends AppCompatActivity v4 (Expense) */
 
 
     /** Реализация с extends ListActivity : */
@@ -83,8 +266,8 @@ public class MainActivity extends /* ListActivity */  AppCompatActivity {
     /** !Реализация с extends AppCompatActivity v2 */
 
 
-    /** Реализация с extends AppCompatActivity v3 : */
-//    /*
+    /** Реализация с extends AppCompatActivity v3 : (Рабочая v1) */
+    /*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -149,8 +332,110 @@ public class MainActivity extends /* ListActivity */  AppCompatActivity {
 
         adapter.notifyDataSetChanged();
     }
-//    */
+    */
     /** !Реализация с extends AppCompatActivity v3 */
+
+
+    /** Реализация с extends AppCompatActivity v4 : (UserAccount) */
+    /*
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.expenseList);
+
+
+        UserAccount tom = new UserAccount("Tom","admin");
+        UserAccount jerry = new UserAccount("Jerry","user");
+        UserAccount donald = new UserAccount("Donald","guest", false);
+
+//        UserAccount[] users = new UserAccount[]{tom,jerry, donald};
+        ArrayList<UserAccount> users = new ArrayList<UserAccount>() {{ add(tom); add(jerry); add(donald); }};
+
+
+        // android.R.layout.simple_list_item_1 is a constant predefined layout of Android.
+        // used to create a ListView with simple ListItem (Only one TextView).
+
+//        ArrayAdapter<UserAccount> arrayAdapter = new ArrayAdapter<UserAccount>(this, android.R.layout.simple_list_item_1 , users);
+        arrayAdapter = new ArrayAdapter <UserAccount> (this, android.R.layout.simple_list_item_1 , users);
+
+        listView.setAdapter(arrayAdapter);
+
+
+        // Обработка установки и снятия отметки в списке :
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+
+//                CheckBox cb = (CheckBox) v.getTag();
+//                cb.setChecked(true);
+
+
+                // получаем нажатый элемент
+                UserAccount user = arrayAdapter.getItem(position);
+
+                if(listView.isItemChecked(position)) {
+                    selectedUsers.add(user);
+                } else {
+                    selectedUsers.remove(user);
+                }
+
+
+//                // снимаем все ранее установленные отметки (Убрать после написания Multiple реализации)
+//                listView.clearChoices();
+
+            }
+
+        });
+        // !Обработка установки и снятия отметки в списке
+
+    }
+
+
+    public void add(View view){
+
+        EditText expenseNameEditText = findViewById(R.id.editTextNameExpense);
+        String expenseName = expenseNameEditText.getText().toString();
+
+        if(!expenseName.isEmpty()){
+
+            UserAccount newExpense = new UserAccount(expenseName,"guest");
+
+
+            arrayAdapter.add(newExpense);
+            expenseNameEditText.setText("");
+            arrayAdapter.notifyDataSetChanged();
+
+
+//            arrayAdapter.getItems();
+//            ArrayList<String> base = new ArrayList<String>(); for (int i = 0; i < adapter.getCount(); i++) base.add(adapter.getItem(i))
+//            arrayAdapter = new ArrayAdapter <UserAccount> (this, android.R.layout.simple_list_item_1 , users);
+
+
+        }
+
+    }
+
+
+    public void remove(View view){
+
+        // получаем и удаляем выделенные элементы
+        for(int i=0; i< selectedUsers.size();i++){
+            arrayAdapter.remove(selectedUsers.get(i));
+        }
+
+        // снимаем все ранее установленные отметки
+        listView.clearChoices();
+
+        // очищаем массив выбраных объектов
+        selectedUsers.clear();
+
+        arrayAdapter.notifyDataSetChanged();
+    }
+    */
+    /** !Реализация с extends AppCompatActivity v4 (UserAccount) */
 
 
     /*
---
+++ app/src/main/java/com/example/test3/expenseList/Expense.java
@@ -0,0 +1,107 @@
+package com.example.test3.expenseList;
+
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+
+public class Expense implements Serializable {
+
+    private String name;
+    private String description;
+    private ZonedDateTime dateTime;
+
+    private ArrayList<Double> expenseList;
+
+
+    /** Необходимое для UI : */
+    private boolean isDeleted;
+    private Integer rowColor;
+
+
+    public Expense() {}
+
+
+    public Expense(String expenseName)  {
+        this.name= expenseName;
+        this.isDeleted = false;
+        this.dateTime = ZonedDateTime.now();
+    }
+
+
+    public Expense(String expenseName, String expenseDescription)  {
+        this.name= expenseName;
+        this.description = expenseDescription;
+        this.isDeleted = false;
+        this.dateTime = ZonedDateTime.now();
+    }
+
+
+    public Expense(String expenseName, String expenseDescription, boolean isDeleted)  {
+        this.name= expenseName;
+        this.description = expenseDescription;
+        this.isDeleted = isDeleted;
+        this.dateTime = ZonedDateTime.now();
+    }
+
+
+    public Expense(String name, String descripton, double expense) {
+        this.name = name;
+        this.description = descripton;
+        this.expenseList = new ArrayList<>();
+        this.expenseList.add(expense);
+        this.dateTime = ZonedDateTime.now();
+    }
+
+
+    public Expense(String name, String description, double expense, ZonedDateTime expenseDateTime) {
+        this.name = name;
+        this.description = description;
+        this.expenseList = new ArrayList<>();
+        this.expenseList.add(expense);
+        this.dateTime = expenseDateTime;
+    }
+
+
+    public String getName() {return name;}
+    public void setName(String name) {this.name = name;}
+
+    public String getDescription() {return description;}
+    public void setDescription(String descripton) {this.description = description;}
+
+    public ArrayList<Double> getExpenseList() {return expenseList;}
+    public void setExpenseList(ArrayList<Double> expenseList) {this.expenseList = expenseList;}
+    public void addExpense(double expense) {
+        if(this.expenseList == null) this.expenseList = new ArrayList<>();
+        this.expenseList.add(expense);
+    }
+    public double getExpenseListTotalAmount() {
+        if(this.expenseList == null) return 0.0;
+        double totalAmount = 0.0;
+        for (Double expense : this.expenseList) {
+            totalAmount = totalAmount + expense;
+        }
+        return totalAmount;
+    }
+    public String getExpenseListTotalAmountString() {
+        return String.valueOf(getExpenseListTotalAmount());
+    }
+
+    public boolean isDeleted() {return isDeleted;}
+    public void setDeleted(boolean deleted) {isDeleted = deleted;}
+
+    public ZonedDateTime getDateTime() {return dateTime;}
+    public void setDateTime(ZonedDateTime dateTime) {this.dateTime = dateTime;}
+
+    public Integer getRowColor() {return rowColor;}
+    public void setRowColor(Integer rowColor) {this.rowColor = rowColor;}
+
+
+    @Override
+    public String toString() {
+        return this.name
+//                .concat(" (").concat(this.descripton == null ? "-" : this.descripton).concat(")")
+                .concat(this.description == null ? "" : " (".concat(this.description).concat(")"))
+                .concat(", Total Amount = ").concat(getExpenseListTotalAmountString());
+    }
+
+}
